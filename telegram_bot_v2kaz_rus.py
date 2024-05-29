b6509f828f3b13adbefcbf45b92446d94112f6ed
Valentin Kim <valentink@yandex.ru>
2024-05-29 18:54:23 +0500

    initial commit

--- telegram_bot_v2kaz_rus.py
+++ telegram_bot_v2kaz_rus.py
@@ -17,47 +17,47 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
-YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN")
+# YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
+# YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN")
 # YANDEX_IAM_TOKEN = os.getenv("YANDEX_IAM_TOKEN")
 
 bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher()
 router = Router()
 
-YANDEX_IAM_TOKEN = None
+# YANDEX_IAM_TOKEN = None
 
 
-def get_iam_token():
-    global YANDEX_IAM_TOKEN
-    url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
-    payload = {"yandexPassportOauthToken": YANDEX_OAUTH_TOKEN}
-    response = requests.post(url, json=payload)
-    response.raise_for_status()
-    YANDEX_IAM_TOKEN = response.json()["iamToken"]
-    logger.info(f"Received new IAM token: {YANDEX_IAM_TOKEN}")
-
-async def refresh_iam_token():
-    while True:
-        await asyncio.sleep(6 * 3600)
-        get_iam_token()
-        logger.info("IAM token refreshed")
-
-def fetch_medicine_info(sku):
-    """Отправка запроса к API для получения данных о лекарстве."""
-    url = 'https://prod-backoffice.daribar.com/api/v1/products/search?city=%D0%90%D0%BB%D0%BC%D0%B0%D1%82%D1%8B'
-    headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
-        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lciI6ZmFsc2UsImV4cCI6MTcxNTcwODM0NiwibmFtZSI6IiIsInBob25lIjoiNzc3NzU4NDY5NjEiLCJyZWZyZXNoIjpmYWxzZSwicm9sZSI6ImFkbWluIiwic2Vzc2lvbl9pZCI6IjE0MGE4ZTg3LWQzZTctNGE4Yi1iODE1LTEyZjE2YjBiZGU0NiJ9.JI2N8d93qDcH5sTOOI0bAo3aRJcjK02ZsMjez_Xd3wQ',
-        'content-type': 'application/json',
-        'origin': 'https://daribar.kz',
-        'referer': 'https://daribar.kz/'
-    }
-    data = f'[{{"sku":"{sku}","count_desired":1}}]'
-    response = requests.post(url, headers=headers, data=data)
-    response.raise_for_status()
-    return response.json()
+# def get_iam_token():
+#     global YANDEX_IAM_TOKEN
+#     url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+#     payload = {"yandexPassportOauthToken": YANDEX_OAUTH_TOKEN}
+#     response = requests.post(url, json=payload)
+#     response.raise_for_status()
+#     YANDEX_IAM_TOKEN = response.json()["iamToken"]
+#     logger.info(f"Received new IAM token: {YANDEX_IAM_TOKEN}")
+#
+# async def refresh_iam_token():
+#     while True:
+#         await asyncio.sleep(6 * 3600)
+#         get_iam_token()
+#         logger.info("IAM token refreshed")
+#
+# def fetch_medicine_info(sku):
+#     """Отправка запроса к API для получения данных о лекарстве."""
+#     url = 'https://prod-backoffice.daribar.com/api/v1/products/search?city=%D0%90%D0%BB%D0%BC%D0%B0%D1%82%D1%8B'
+#     headers = {
+#         'accept': 'application/json, text/plain, */*',
+#         'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
+#         'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lciI6ZmFsc2UsImV4cCI6MTcxNTcwODM0NiwibmFtZSI6IiIsInBob25lIjoiNzc3NzU4NDY5NjEiLCJyZWZyZXNoIjpmYWxzZSwicm9sZSI6ImFkbWluIiwic2Vzc2lvbl9pZCI6IjE0MGE4ZTg3LWQzZTctNGE4Yi1iODE1LTEyZjE2YjBiZGU0NiJ9.JI2N8d93qDcH5sTOOI0bAo3aRJcjK02ZsMjez_Xd3wQ',
+#         'content-type': 'application/json',
+#         'origin': 'https://daribar.kz',
+#         'referer': 'https://daribar.kz/'
+#     }
+#     data = f'[{{"sku":"{sku}","count_desired":1}}]'
+#     response = requests.post(url, headers=headers, data=data)
+#     response.raise_for_status()
+#     return response.json()
 
 
 @router.message(CommandStart())
@@ -98,121 +98,121 @@ async def handle_voice_message(message: types.Message):
 
 
 
-
-
-@router.message()
-async def handle_any_message(message: Message):
-    user_id = message.from_user.id
-    user_language = user_languages.get(user_id, 'ru')
-    if user_language == 'kk':
-        await message.answer(text="Маған бір дәрі-дәрмектің атауы бар дауыстық хабарлама жіберіңіз (демонстрацияға рұқсат етілген дәрілік нұсқалар: ибупрофен, анальгин, аспирин, қызыл май).\nСонымен қатар демонстрациялық бейнені мына сілтемеден көре аласыз:\nhttps://youtube.com/shorts/CLDUrzblecI?feature=share")
-    else:
-        await message.answer(
-            text="Отправьте мне, пожалуйста, голосовое сообщение с названием одного препарата (допустимые к демонстрации препараты: Ибупрофен, Анальгин, Аспирин, Кызыл май).\nТакже посмотреть демонстрационное видео можно по этой ссылке:\nhttps://youtube.com/shorts/CLDUrzblecI?feature=share")
-
-
-def format_response(data):
-    results = []
-    for item in data['result'][:3]:
-        pharmacy_info = f"Препарат {item['products'][0]['name']}\nАдрес:\nгород {item['source']['city']},\n{item['source']['address']}\nCтоимость препарата {item['products'][0]['base_price']} тенге\n"
-        results.append(pharmacy_info)
-    return "\n".join(results)
-
-
-def remove_annotations(text: str) -> str:
-    pattern = r'(\[\[.*?\]\])|(\【[^】]*\】)'
-    cleaned_text = re.sub(pattern, '', text)
-    return cleaned_text
-
-
-def translate_text(text, source_lang='ru', target_lang='kk'):
-    url = "https://translate.api.cloud.yandex.net/translate/v2/translate"
-    headers = {
-        'Authorization': f'Bearer {YANDEX_IAM_TOKEN}',
-        'Content-Type': 'application/json'
-    }
-    payload = {
-        'folder_id': YANDEX_FOLDER_ID,
-        'texts': [text],
-        'targetLanguageCode': target_lang,
-        'sourceLanguageCode': source_lang
-    }
-    try:
-        response = requests.post(url, json=payload, headers=headers)
-        response.raise_for_status()
-        translations = response.json().get('translations', [])
-        if translations:
-            return translations[0]['text']
-        else:
-            return "Перевод не найден."
-    except requests.exceptions.HTTPError as http_err:
-        logging.error(f"HTTP error occurred: {http_err}")
-        return "Перевод не удался."
-    except Exception as err:
-        logging.error(f"An error occurred: {err}")
-        return "Перевод не удался."
-
-
-def recognize_speech(audio_content):
-    global YANDEX_IAM_TOKEN
-    url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={YANDEX_FOLDER_ID}&lang=ru-RU"
-    headers = {"Authorization": f"Bearer {YANDEX_IAM_TOKEN}"}
-
-    logger.info(f"Sending {len(audio_content)} bytes to Yandex STT.")
-    response = requests.post(url, headers=headers, data=audio_content)
-    response.raise_for_status()
-
-    if response.status_code == 200:
-        result = response.json().get("result")
-        logger.info(f"Recognition result: {result}")
-        return result
-    else:
-        error_message = f"Failed to recognize speech, status code: {response.status_code}"
-        logger.error(error_message)
-        raise Exception(error_message)
-
-
-def synthesize_speech(text, lang_code):
-    voice_settings = {
-        'ru': {'lang': 'ru-RU', 'voice': 'oksana'},
-        'kk': {'lang': 'kk-KK', 'voice': 'amira'}
-    }
-    settings = voice_settings.get(lang_code, voice_settings['ru'])
-
-    url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
-    headers = {'Authorization': f'Bearer {YANDEX_IAM_TOKEN}'}
-    data = {
-        'text': text,
-        'lang': settings['lang'],
-        'voice': settings['voice'],
-        'folderId': YANDEX_FOLDER_ID,
-        'format': 'mp3',
-        'sampleRateHertz': 48000,
-    }
-
-    response = requests.post(url, headers=headers, data=data, stream=True)
-    logger.info(f"Status Code: {response.status_code}")
-    logger.info(f"Response Headers: {response.headers}")
-
-    if response.status_code == 200:
-        audio_content = response.content
-        logger.info(f"Received audio content length: {len(audio_content)} bytes")
-        with open("response.mp3", "wb") as file:
-            file.write(audio_content)
-        logger.info("Audio content saved as 'response.mp3'.")
-        return audio_content
-    else:
-        error_message = f"Failed to synthesize speech, status code: {response.status_code}, response text: {response.text}"
-        logger.error(error_message)
-        raise Exception(error_message)
-
+#
+#
+# @router.message()
+# async def handle_any_message(message: Message):
+#     user_id = message.from_user.id
+#     user_language = user_languages.get(user_id, 'ru')
+#     if user_language == 'kk':
+#         await message.answer(text="Маған бір дәрі-дәрмектің атауы бар дауыстық хабарлама жіберіңіз (демонстрацияға рұқсат етілген дәрілік нұсқалар: ибупрофен, анальгин, аспирин, қызыл май).\nСонымен қатар демонстрациялық бейнені мына сілтемеден көре аласыз:\nhttps://youtube.com/shorts/CLDUrzblecI?feature=share")
+#     else:
+#         await message.answer(
+#             text="Отправьте мне, пожалуйста, голосовое сообщение с названием одного препарата (допустимые к демонстрации препараты: Ибупрофен, Анальгин, Аспирин, Кызыл май).\nТакже посмотреть демонстрационное видео можно по этой ссылке:\nhttps://youtube.com/shorts/CLDUrzblecI?feature=share")
+#
+#
+# def format_response(data):
+#     results = []
+#     for item in data['result'][:3]:
+#         pharmacy_info = f"Препарат {item['products'][0]['name']}\nАдрес:\nгород {item['source']['city']},\n{item['source']['address']}\nCтоимость препарата {item['products'][0]['base_price']} тенге\n"
+#         results.append(pharmacy_info)
+#     return "\n".join(results)
+#
+#
+# def remove_annotations(text: str) -> str:
+#     pattern = r'(\[\[.*?\]\])|(\【[^】]*\】)'
+#     cleaned_text = re.sub(pattern, '', text)
+#     return cleaned_text
+#
+#
+# def translate_text(text, source_lang='ru', target_lang='kk'):
+#     url = "https://translate.api.cloud.yandex.net/translate/v2/translate"
+#     headers = {
+#         'Authorization': f'Bearer {YANDEX_IAM_TOKEN}',
+#         'Content-Type': 'application/json'
+#     }
+#     payload = {
+#         'folder_id': YANDEX_FOLDER_ID,
+#         'texts': [text],
+#         'targetLanguageCode': target_lang,
+#         'sourceLanguageCode': source_lang
+#     }
+#     try:
+#         response = requests.post(url, json=payload, headers=headers)
+#         response.raise_for_status()
+#         translations = response.json().get('translations', [])
+#         if translations:
+#             return translations[0]['text']
+#         else:
+#             return "Перевод не найден."
+#     except requests.exceptions.HTTPError as http_err:
+#         logging.error(f"HTTP error occurred: {http_err}")
+#         return "Перевод не удался."
+#     except Exception as err:
+#         logging.error(f"An error occurred: {err}")
+#         return "Перевод не удался."
+#
+#
+# def recognize_speech(audio_content):
+#     global YANDEX_IAM_TOKEN
+#     url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={YANDEX_FOLDER_ID}&lang=ru-RU"
+#     headers = {"Authorization": f"Bearer {YANDEX_IAM_TOKEN}"}
+#
+#     logger.info(f"Sending {len(audio_content)} bytes to Yandex STT.")
+#     response = requests.post(url, headers=headers, data=audio_content)
+#     response.raise_for_status()
+#
+#     if response.status_code == 200:
+#         result = response.json().get("result")
+#         logger.info(f"Recognition result: {result}")
+#         return result
+#     else:
+#         error_message = f"Failed to recognize speech, status code: {response.status_code}"
+#         logger.error(error_message)
+#         raise Exception(error_message)
+#
+#
+# def synthesize_speech(text, lang_code):
+#     voice_settings = {
+#         'ru': {'lang': 'ru-RU', 'voice': 'oksana'},
+#         'kk': {'lang': 'kk-KK', 'voice': 'amira'}
+#     }
+#     settings = voice_settings.get(lang_code, voice_settings['ru'])
+#
+#     url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
+#     headers = {'Authorization': f'Bearer {YANDEX_IAM_TOKEN}'}
+#     data = {
+#         'text': text,
+#         'lang': settings['lang'],
+#         'voice': settings['voice'],
+#         'folderId': YANDEX_FOLDER_ID,
+#         'format': 'mp3',
+#         'sampleRateHertz': 48000,
+#     }
+#
+#     response = requests.post(url, headers=headers, data=data, stream=True)
+#     logger.info(f"Status Code: {response.status_code}")
+#     logger.info(f"Response Headers: {response.headers}")
+#
+#     if response.status_code == 200:
+#         audio_content = response.content
+#         logger.info(f"Received audio content length: {len(audio_content)} bytes")
+#         with open("response.mp3", "wb") as file:
+#             file.write(audio_content)
+#         logger.info("Audio content saved as 'response.mp3'.")
+#         return audio_content
+#     else:
+#         error_message = f"Failed to synthesize speech, status code: {response.status_code}, response text: {response.text}"
+#         logger.error(error_message)
+#         raise Exception(error_message)
+#
 
 dp.include_router(router)
 
 
 async def main():
-    get_iam_token()
-    task = asyncio.create_task(refresh_iam_token())
+    # get_iam_token()
+    # task = asyncio.create_task(refresh_iam_token())
     _ = task
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, skip_updates=True)
